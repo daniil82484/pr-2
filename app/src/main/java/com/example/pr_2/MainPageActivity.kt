77c86a70bb398dd6d37dd38cd74fa07979b03cf5
danii <daniil82484@gmail.com>
2023-04-14 16:09:54 +0500

    2

--- app/src/main/java/com/example/pr_2/MainPageActivity.kt
+++ app/src/main/java/com/example/pr_2/MainPageActivity.kt
@@ -1,15 +1,25 @@
 package com.example.pr_2
 
+import android.content.DialogInterface
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
 import androidx.appcompat.app.AppCompatActivity
 
 
 class MainPageActivity : AppCompatActivity() {
+
+    var modalBottomSheet = ModalBottomSheet()
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main_page)
-
-
     }
 
+    fun close(view: View) {
+        modalBottomSheet.dismiss()
+    }
+
+    fun modalBottomSheet(view: View) {
+        modalBottomSheet.show(supportFragmentManager, ModalBottomSheet.TAG)
+    }
 }
---
+++ app/src/main/java/com/example/pr_2/ModalBottomSheet.kt
@@ -0,0 +1,22 @@
+package com.example.pr_2
+
+import android.os.Bundle
+import com.google.android.material.bottomsheet.BottomSheetDialogFragment
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+
+class ModalBottomSheet : BottomSheetDialogFragment() {
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? = inflater.inflate(R.layout.modal_bottom_sheet, container, false)
+
+    companion object {
+        const val TAG = "ModalBottomSheet"
+    }
+
+
+}
